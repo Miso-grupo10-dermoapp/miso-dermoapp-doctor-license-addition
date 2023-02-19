e2f15594e3e949fda40faba9407a818dc95f08ac
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-18 21:02:45 -0500

    Feature/dg 73 adding doctor license (#2)
    * feature initialization test
    
    * testing event var
    
    * logic with happy path test
    
    * complete service and integration
    
    * fixing db name
    
    * adding pk
    
    * change db name
    
    * unit testing and last improvements
    
    * preparing PR changes
    
    * adding init file for src designation
    
    * setting correct org
    
    * sonar missing config key
    
    * adding sonar token keys
    
    * refactoring secrets keys
    
    * triggering repo
    
    * testing
    
    * change project key settings
    
    * fixing sonar issues
    
    * fixing CI
    
    * code smells fixes
    
    * adding CORS
    
    * restoring deploy

--- src/db_service.py
+++ src/db_service.py
@@ -1,36 +1,31 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 
-client = boto3.resource('dynamodb')
+import app
 
 
 def insert_item(body):
+
     try:
-        table = client.Table('dermoapp-v2-Doctor-AZEUNS8HXUN1')
-        data = table.put_item(
-            Item={
-                'id': '45678',
-                'license': '655756565',
-                'specialialties': [
-                    {
-                        "name": "dermatologist",
-                        "verified": True
-                    }
-                ]
-            }
-        )
-        return data
+        client = boto3.resource("dynamodb")
+        table = client.Table(app.ENV_TABLE_NAME)
+        table.put_item(Item=body)
+        return True
     except Exception as e:
-        return 'cannot persist on db cause: ' + str(e)
+        raise RuntimeError('cannot persist on db cause: ' + str(e))
 
 
-def get_item():
+def get_item(key, value):
+    client = boto3.resource('dynamodb')
     try:
-        table = client.Table('dermoapp-v2-Doctor-AZEUNS8HXUN1')
-        data = table.get_item(
-            Key={
-                'id': '123sdsdsd'
-            }
+        table = client.Table(app.ENV_TABLE_NAME)
+        response = table.query(
+            KeyConditionExpression=Key(key).eq(value)
         )
-        return data
+        items = response['Items']
+        if items:
+            return items[0]
+        else:
+            return []
     except Exception as e:
-        return 'cannot persist on db cause: ' + str(e)
+        raise RuntimeError('cannot retrieve data from db cause: ' + str(e))
--- test/lambda_function_test.py
+++
@@ -1,7 +0,0 @@
-from src.app import *
-
-
-class TestPacientDetailService:
-
-    def test_givenLambdaFunctionCallThenReturnJsonResponse(self):
-        assert 1==1
